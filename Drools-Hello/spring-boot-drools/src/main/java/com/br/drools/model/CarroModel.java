4aa5fc317c0ad52b6c363623bf05bec80bcd082b
EDUARDO NOFRE REIS DE SA <eduardo.sa@gps-pamcary.local>
2020-07-23 18:59:21 -0300

    adicionando o lombok

--- Drools-Hello/spring-boot-drools/src/main/java/com/br/drools/model/CarroModel.java
+++ Drools-Hello/spring-boot-drools/src/main/java/com/br/drools/model/CarroModel.java
@@ -1,33 +1,13 @@
 package com.br.drools.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CarroModel {
 
 	private String marca;
 	private String modelo;
 	private int ano;
-
-	public String getMarca() {
-		return marca;
-	}
-
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-
-	public String getModelo() {
-		return modelo;
-	}
-
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
-
-	public int getAno() {
-		return ano;
-	}
-
-	public void setAno(int ano) {
-		this.ano = ano;
-	}
-
 }
--- Drools-Hello/spring-boot-drools/src/main/java/com/br/drools/model/PessoaModel.java
+++ Drools-Hello/spring-boot-drools/src/main/java/com/br/drools/model/PessoaModel.java
@@ -1,33 +1,14 @@
 package com.br.drools.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PessoaModel {
 
 	private String nome;
 	private int idade;
 	private String docCpf;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public int getIdade() {
-		return idade;
-	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-	public String getDocCpf() {
-		return docCpf;
-	}
-
-	public void setDocCpf(String docCpf) {
-		this.docCpf = docCpf;
-	}
 
 }
